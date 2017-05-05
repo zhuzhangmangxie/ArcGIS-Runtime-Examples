becc3f75ecd153f94cd0415f1d3f6d235b8bcd85
zhuzhangmangxie <lifei.gisdeveloper@gmail.com>
2017-05-05 11:56:30 +0800

    示例
    第一个arcgis runtime for wpf的demo

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            MyMap.Layers.LayersInitialized += (o, evt) =>
+            {
+                IsBusy = false;
+
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isBusy = true;
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            set
+            {
+                _isBusy = value;
+
+                if (PropertyChanged != null)
+                {
+
+                    PropertyChanged(this, new PropertyChangedEventArgs("IsBusy"));
+                }
+            }
+        }
+
+    }
+
+}
